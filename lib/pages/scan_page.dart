fd75e02ee3b74c0ef0d05d650be96b1efad76a17
punkushal <spycymagar@gmail.com>
2024-12-08 19:48:38 +0545

    Scan Page Created

---
+++ lib/pages/scan_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ScanPage extends StatefulWidget {
+  static const String routeName = 'scan';
+  const ScanPage({super.key});
+
+  @override
+  State<ScanPage> createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan Page'),
+      ),
+    );
+  }
+}
