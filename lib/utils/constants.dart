8f974f5280685a5c681fdce1a5660db1049b9962
punkushal <spycymagar@gmail.com>
2024-12-11 16:48:13 +0545

    Constants For Contact

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+abstract final class ContactProperties {
+  static const String name = 'Name';
+  static const String company = 'Company Name';
+  static const String mobile = 'Mobile';
+  static const String address = 'Address';
+  static const String email = 'Email';
+  static const String website = 'Website';
+  static const String designation = 'Designation';
+}
