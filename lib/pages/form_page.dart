9db7da90f8a4c1c9103fd5d058ba40014be361ab
punkushal <spycymagar@gmail.com>
2024-12-12 13:59:54 +0545

    Form Page Is Added

---
+++ lib/pages/form_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:virtual_card/models/contact_model.dart';
+
+class FormPage extends StatefulWidget {
+  static const String routeName = 'form';
+  final ContactModel contactModel;
+  const FormPage({super.key, required this.contactModel});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.contactModel.company),
+      ),
+    );
+  }
+}
