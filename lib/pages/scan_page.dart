6d5e3ac42cc56c24743f8835082981d0d55c25a5
punkushal <spycymagar@gmail.com>
2024-12-12 10:53:57 +0545

    Drag Item & Drop Widget Added

--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:virtual_card/utils/constants.dart';
 
 class ScanPage extends StatefulWidget {
   static const String routeName = 'scan';
@@ -15,6 +16,7 @@ class ScanPage extends StatefulWidget {
 
 class _ScanPageState extends State<ScanPage> {
   List<String> textLines = [];
+  bool isScanOver = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +44,37 @@ class _ScanPageState extends State<ScanPage> {
               ),
             ],
           ),
+
+          //Drop Down Item Will Be Dropped Here..
+          if (isScanOver)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 1,
+                color: Colors.amber.shade100,
+                shadowColor: Colors.grey[300],
+                child: Column(
+                  children: [
+                    DropTargetItem(
+                        property: ContactProperties.company,
+                        onDrop: getDropItem),
+                    DropTargetItem(
+                        property: ContactProperties.name, onDrop: getDropItem),
+                    DropTargetItem(
+                        property: ContactProperties.designation,
+                        onDrop: getDropItem),
+                    DropTargetItem(
+                        property: ContactProperties.address,
+                        onDrop: getDropItem),
+                    DropTargetItem(
+                        property: ContactProperties.email, onDrop: getDropItem),
+                    DropTargetItem(
+                        property: ContactProperties.mobile,
+                        onDrop: getDropItem),
+                  ],
+                ),
+              ),
+            ),
 
           //For Displaying Extracted Texts From Image
           Wrap(
@@ -79,9 +112,12 @@ class _ScanPageState extends State<ScanPage> {
       }
       setState(() {
         textLines = tempList;
+        isScanOver = true;
       });
     }
   }
+
+  getDropItem(String property, String value) {}
 }
 
 class LineItem extends StatelessWidget {
@@ -108,6 +144,78 @@ class LineItem extends StatelessWidget {
         child: Chip(
           label: Text(line),
         ),
+      ),
+    );
+  }
+}
+
+//Drop Item Widget
+class DropTargetItem extends StatefulWidget {
+  final String property;
+  final Function(String, String) onDrop;
+  const DropTargetItem(
+      {super.key, required this.property, required this.onDrop});
+
+  @override
+  State<DropTargetItem> createState() => _DropTargetItemState();
+}
+
+class _DropTargetItemState extends State<DropTargetItem> {
+  String dropItem = '';
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Text(widget.property),
+          ),
+          Expanded(
+            flex: 2,
+            child: DragTarget(
+              builder: (context, candidateData, rejectedData) => Container(
+                decoration: BoxDecoration(
+                  border: candidateData.isNotEmpty
+                      ? Border.all(
+                          width: 2,
+                          color: Colors.green.shade300,
+                        )
+                      : null,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        dropItem.isEmpty
+                            ? 'drop ${widget.property.toLowerCase()}'
+                            : dropItem,
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    if (dropItem.isNotEmpty)
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            dropItem = '';
+                          });
+                        },
+                        icon: const Icon(Icons.close),
+                      ),
+                  ],
+                ),
+              ),
+              onAcceptWithDetails: (data) {
+                if (dropItem.isEmpty) {
+                  dropItem = data.data as String;
+                } else {
+                  dropItem += ' ${data.data as String}';
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
