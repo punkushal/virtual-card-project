b5d94aa04cae49aba5d175a8b53e45d0659781a0
punkushal <spycymagar@gmail.com>
2024-12-12 17:30:15 +0545

    Padding Added

--- lib/widgets/app_txt_form_field.dart
+++ lib/widgets/app_txt_form_field.dart
@@ -20,37 +20,40 @@ class AppTxtFormField extends StatelessWidget {
   final double radiusValue;
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: labelText,
-        prefixIcon: prefixIcon,
-        contentPadding:
-            EdgeInsets.symmetric(vertical: vertical, horizontal: horizontal),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.red.shade300),
-          borderRadius: BorderRadius.all(
-            Radius.circular(radiusValue),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: labelText,
+          prefixIcon: prefixIcon,
+          contentPadding:
+              EdgeInsets.symmetric(vertical: vertical, horizontal: horizontal),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.green),
-          borderRadius: BorderRadius.all(
-            Radius.circular(radiusValue),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.red.shade300),
+            borderRadius: BorderRadius.all(
+              Radius.circular(radiusValue),
+            ),
           ),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.green.shade200),
-          borderRadius: BorderRadius.all(
-            Radius.circular(radiusValue),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.green),
+            borderRadius: BorderRadius.all(
+              Radius.circular(radiusValue),
+            ),
           ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green.shade200),
+            borderRadius: BorderRadius.all(
+              Radius.circular(radiusValue),
+            ),
+          ),
+          errorStyle: const TextStyle(color: Colors.redAccent),
         ),
-        errorStyle: const TextStyle(color: Colors.redAccent),
+        validator: validator,
       ),
-      validator: validator,
     );
   }
 }
