3c1af740c8829f4e6e6c9d11f1e96efb9891ee35
punkushal <spycymagar@gmail.com>
2024-12-13 10:53:13 +0545

    Modify ContactModel Class For Using Database

--- lib/models/contact_model.dart
+++ lib/models/contact_model.dart
@@ -1,3 +1,18 @@
+//Table Name For The Database
+const String tblContact = 'tbl_contact';
+
+//Column Names For The Table
+const String tblCtcColName = 'Name';
+const String tblCtcColId = 'Id';
+const String tblCtcColCompany = 'Company';
+const String tblCtcColEmail = 'Email';
+const String tblCtcColAddress = 'Address';
+const String tblCtcColMobile = 'Mobile';
+const String tblCtcColDesignation = 'Designation';
+const String tblCtcColFavorite = 'Favorite';
+const String tblCtcColWebsite = 'Website';
+const String tblCtcColImage = 'Image';
+
 class ContactModel {
   int id;
   String name;
@@ -22,4 +37,40 @@ class ContactModel {
     this.website = '',
     this.favorite = false,
   });
+
+  //Map Value For The Database
+  Map<String, dynamic> toMap() {
+    final map = <String, dynamic>{
+      tblCtcColName: name,
+      tblCtcColCompany: company,
+      tblCtcColAddress: address,
+      tblCtcColDesignation: designation,
+      tblCtcColEmail: email,
+      tblCtcColImage: imageUrl,
+      tblCtcColWebsite: website,
+      tblCtcColMobile: mobile,
+
+      //Sqflite package doesn't support boolean type
+      tblCtcColFavorite: favorite ? 1 : 0,
+    };
+
+    if (id > 0) {
+      map[tblCtcColId] = id;
+    }
+
+    return map;
+  }
+
+  //To Convert From Map To Contact Model
+  factory ContactModel.fromMap(Map<String, dynamic> map) => ContactModel(
+        name: map[tblCtcColName],
+        mobile: map[tblCtcColMobile],
+        company: map[tblCtcColCompany],
+        address: map[tblCtcColAddress],
+        email: map[tblCtcColEmail],
+        designation: map[tblCtcColDesignation],
+        website: map[tblCtcColWebsite],
+        imageUrl: map[tblCtcColImage],
+        favorite: map[tblCtcColFavorite] == 1 ? true : false,
+      );
 }
