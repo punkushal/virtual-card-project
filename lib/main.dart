c107e3acf49a0bb83b37f0abd19070015a407728
punkushal <spycymagar@gmail.com>
2024-12-13 13:53:43 +0545

    Root Widget Wrapped With Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:virtual_card/models/contact_model.dart';
 import 'package:virtual_card/pages/form_page.dart';
 import 'package:virtual_card/pages/home_page.dart';
 import 'package:virtual_card/pages/scan_page.dart';
+import 'package:virtual_card/providers/contact_provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ContactProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
