70fb15043d7ae7ea984575e0173458330ba703bb
punkushal <spycymagar@gmail.com>
2024-12-14 08:21:40 +0545

    Refresh The List To Display After Contact Is Inserted

--- lib/providers/contact_provider.dart
+++ lib/providers/contact_provider.dart
@@ -7,8 +7,12 @@ class ContactProvider extends ChangeNotifier {
   final db = DbHelper();
 
   //To insert new contact infor to db
-  Future<int> insertContact(ContactModel contactModel) {
-    return db.insertContact(contactModel);
+  Future<int> insertContact(ContactModel contactModel) async {
+    final rowId = await db.insertContact(contactModel);
+    contactModel.id = rowId;
+    allContactList.insert(0, contactModel);
+    notifyListeners();
+    return rowId;
   }
 
   //To fetch all the contactlists from db
