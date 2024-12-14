a545a91851e923f97bc01495b609d1bd1b461680
punkushal <spycymagar@gmail.com>
2024-12-14 08:08:43 +0545

    Duration & Dismiss Direction Added

--- lib/utils/helper_function.dart
+++ lib/utils/helper_function.dart
@@ -4,5 +4,7 @@ showMsg(BuildContext context, String msg) =>
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(msg),
+        duration: const Duration(seconds: 3),
+        dismissDirection: DismissDirection.down,
       ),
     );
