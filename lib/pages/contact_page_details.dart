f3a437074efb7a350bc11fcd83430accaa47fb08
punkushal <spycymagar@gmail.com>
2024-12-15 16:57:56 +0545

    Future Builder Used To Display Contact Detail

--- lib/pages/contact_page_details.dart
+++ lib/pages/contact_page_details.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:virtual_card/models/contact_model.dart';
+import 'package:virtual_card/providers/contact_provider.dart';
 
 class ContactPageDetails extends StatefulWidget {
   static const String routeName = 'contact';
@@ -10,8 +15,46 @@ class ContactPageDetails extends StatefulWidget {
 }
 
 class _ContactPageDetailsState extends State<ContactPageDetails> {
+  late int id;
+
+  @override
+  void initState() {
+    id = widget.id;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact Page Details'),
+        centerTitle: true,
+      ),
+      body: Consumer<ContactProvider>(builder: (context, provider, child) {
+        return FutureBuilder<ContactModel>(
+          future: provider.getContactById(id),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView(
+                children: [
+                  Image.file(
+                    File(snapshot.data!.imageUrl),
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 250,
+                  ),
+                ],
+              );
+            }
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text('Error While Loading'),
+              );
+            }
+            return const Center(child: Text('Please wait.....'));
+          },
+        );
+      }),
+    );
   }
 }
