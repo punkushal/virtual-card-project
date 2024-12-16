d27f37e361a6baab986eb01b5fac6e745e6dc459
punkushal <spycymagar@gmail.com>
2024-12-16 17:16:41 +0545

    Call & Sms Function Added To Launch Those Url

--- lib/pages/contact_page_details.dart
+++ lib/pages/contact_page_details.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:virtual_card/models/contact_model.dart';
 import 'package:virtual_card/providers/contact_provider.dart';
+import 'package:virtual_card/utils/helper_function.dart';
 
 class ContactPageDetails extends StatefulWidget {
   static const String routeName = 'contact';
@@ -35,14 +37,40 @@ class _ContactPageDetailsState extends State<ContactPageDetails> {
           future: provider.getContactById(id),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
+              final contact = snapshot.data;
               return ListView(
                 children: [
                   Image.file(
-                    File(snapshot.data!.imageUrl),
+                    File(contact!.imageUrl),
                     fit: BoxFit.cover,
                     width: double.infinity,
                     height: 250,
                   ),
+
+                  //For Displaying user's contact info such as mobile number
+                  ListTile(
+                    leading: Text(
+                      contact.mobile,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            getCall(contact.mobile);
+                          },
+                          icon: const Icon(Icons.call),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            getSms(contact.mobile);
+                          },
+                          icon: const Icon(Icons.sms),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               );
             }
@@ -56,5 +84,26 @@ class _ContactPageDetailsState extends State<ContactPageDetails> {
         );
       }),
     );
+  }
+
+  void getCall(String mobile) async {
+    final url = 'tel:$mobile';
+
+    if (await canLaunchUrlString(url)) {
+      launchUrlString(url);
+    } else {
+      // ignore: use_build_context_synchronously
+      showMsg(context, 'cannot perform this task');
+    }
+  }
+
+  void getSms(String mobile) async {
+    final url = 'sms:$mobile';
+    if (await canLaunchUrlString(url)) {
+      launchUrlString(url);
+    } else {
+      // ignore: use_build_context_synchronously
+      showMsg(context, 'cannot perform this task');
+    }
   }
 }
